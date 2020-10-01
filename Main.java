d6aa6ddf168fc55037a54360a2dcf16e64edc355
chadmorris55 <71134162+chadmorris55@users.noreply.github.com>
2020-10-01 12:48:19 -0400

    Update Main.java
    prints the word form of a number that is between 1 and 10 inclusive

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * finds the largest integer out of the three given
+ * @Chad 
  */
 public class Main {
 
@@ -9,7 +10,49 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+  final int threshhold = 10;
+    //asks user for number
+    System.out.println("Please enter a positive integer:");
+    int number = input.nextInt();
+    if (number <= threshhold){
+    switch (number) {
+      case 1:
+        System.out.println("one");
+        break;
+      case 2:
+        System.out.println("two");
+        break;
+      case 3:
+        System.out.println("three");
+        break;
+      case 4:
+        System.out.println("four");
+        break;
+      case 5:
+        System.out.println("five");
+        break;
+      case 6:
+        System.out.println("six");
+        break;
+      case 7:
+        System.out.println("seven");
+        break;
+      case 8:
+        System.out.println("eight");
+        break;
+      case 9:
+        System.out.println("nine");
+        break;
+      case 10:
+        System.out.println("ten");
+        break;
+        
+            }
+    }else{
+      System.out.println("not in the range");
+    }
   }
 }
